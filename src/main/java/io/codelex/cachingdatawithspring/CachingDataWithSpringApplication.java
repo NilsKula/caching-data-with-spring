e509be4e6546279eb3572f03f142cfc72b6d45d7
nils <kula.nils@gmail.com>
2019-04-26 19:18:24 +0300

    setup

---
+++ src/main/java/io/codelex/cachingdatawithspring/CachingDataWithSpringApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.cachingdatawithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingDataWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingDataWithSpringApplication.class, args);
+	}
+
+}
